17b894c344c87263e20e7d4287af63dd2a571029
Steven Meads <sbmeads@gmail.com>
2016-08-04 11:31:15 -0700

    Checkpoint 3:Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
-  resource :users
 
   get 'welcome/index'
-
   get 'welcome/about'
 
   root 'welcome#index'
