fac70f4e118662156cd1661730a048c8881ec7a2
Steven Meads <sbmeads@gmail.com>
2016-08-03 22:48:21 -0700

    Added username field in sign_up

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ source 'https://rubygems.org'
  gem 'bootstrap-sass'
  gem 'figaro'
  gem 'pry'
+ gem 'devise'
 
  group :development, :test do
    gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resource :users
+
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root 'welcome#about'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
