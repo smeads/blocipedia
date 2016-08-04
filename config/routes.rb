4e26a0b3a00269e266a8d17d15c975a7f8e3409c
Steven Meads <sbmeads@gmail.com>
2016-08-04 14:25:39 -0700

    Wiki CRUD: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
+  resources :wikis
 
-  get 'welcome/index'
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
 
