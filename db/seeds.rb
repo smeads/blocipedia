449590b2b8227badeb46eff42242d099cd787ded
Steven Meads <sbmeads@gmail.com>
2016-08-05 16:08:55 -0700

    Seeded Data w/Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# creates users
+10.times do
+  User.create!(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "helloworld",
+    confirmed_at: "2016-08-4 18:00:00"
+  )
+end
+users = User.all
+
+# creates wikis
+20.times do
+  Wiki.create!(
+    title: Faker::Hipster.sentence(2),
+    body: Faker::Hipster.paragraph(4, true, 2),
+    private: false,
+    user_id: rand(User.count)
+  )
+end
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
